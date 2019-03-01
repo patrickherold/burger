7904b2b485f02de584ad901c0a214b6aca2eb6b6
patrickherold <patrick@herold.org>
2019-03-01 14:31:20 -0500

    setup

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burgerName) VALUES ('Fish Lips Burger');
+INSERT INTO burgers (burgerName) VALUES ('Chili Dance Pants Burger');
+INSERT INTO burgers (burgerName, isEaten) VALUES ('Guacamole Burger', true);
---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+
+var port = process.env.PORT || 8080;
+
+var app = express();
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// allows for PUT or DELETE
+app.use(methodOverride("_method"));
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burger_controller.js");
+
+app.use("/", routes);
+
+app.listen(port);
